990082fead41a8cc2a241441687bbf978a4197e9
Tom Dunlap <apocryphalauthor@gmail.com>
2015-03-28 22:29:13 -0400

    [Start #91329134] Refactor `background` to `side`

--- lib/hexflex/triangle.rb
+++ lib/hexflex/triangle.rb
@@ -3,14 +3,14 @@ module Hexflex
     BASE = 1.0
     HEIGHT = 0.5 * BASE * Math.sqrt(3.0)
 
-    attr_accessor :background
+    attr_accessor :face
 
     # make a new triangle
     # @param [Hash] opts options hash with properties for new triangle
-    # @option opts [Mixed] :background the background of the triangle, can be a
-    #   symbol, string, or hex number
+    # @option opts [Mixed] :face the fill of the triangle, can be a
+    #   symbol, string, or hex number representing a color
     def initialize(opts = {})
-      @background = opts[:background]
+      @face = opts[:face]
     end
   end
 end
--- spec/hexflex/triangle_spec.rb
+++ spec/hexflex/triangle_spec.rb
@@ -2,14 +2,15 @@ require 'spec_helper'
 
 describe Hexflex::Triangle do
 
-  it "can be initialized with a background" do
-    backgrounded = Hexflex::Triangle.new(background: :red)
-    expect(backgrounded.background).to eq :red
+  it "can be initialized with a face" do
+    triangle = Hexflex::Triangle.new(face: :red)
+    expect(triangle.face).to eq :red
   end
 
-  it "can have a background" do
-    subject.background = :red
-    expect(subject.background).to eq :red
+  it "can have a face" do
+    triangle = Hexflex::Triangle.new
+    triangle.face = :red
+    expect(triangle.face).to eq :red
   end
 
 end
