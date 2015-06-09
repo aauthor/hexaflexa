3c665f19c7d3c34a626edef5be40cb9c696f98e4
Tom Dunlap <apocryphalauthor@gmail.com>
2015-06-08 20:23:15 -0700

    Handle second row triangle placement

--- lib/hexflex/triangle_placer.rb
+++ lib/hexflex/triangle_placer.rb
@@ -3,12 +3,13 @@ module Hexflex
     attr_reader :triangle_use, :index
     def initialize(triangle_use, index)
       @triangle_use = triangle_use
-      @index = index
+      @index = index.to_i
     end
 
     def place!
       triangle_use.translate(*origin_shift)
       triangle_use.translate(*lateral_placement)
+      triangle_use.translate(*vertical_placement)
       if(index.odd?)
         triangle_use.translate(*pre_odd_rotation_vertical_adjustment)
         triangle_use.rotate(*odd_rotation)
@@ -22,11 +23,11 @@ module Hexflex
     end
 
     def lateral_placement
-      [Hexflex::R * index , 0]
+      [(Hexflex::R * index) % (Hexflex::R * 10) , 0]
     end
 
     def vertical_placement
-      [0, 0]
+      [0, (index/10) * (Hexflex::Y + Hexflex::R)]
     end
 
     def odd_rotation
--- spec/hexflex/triangle_placer_spec.rb
+++ spec/hexflex/triangle_placer_spec.rb
@@ -18,12 +18,6 @@ describe Hexflex::TrianglePlacer do
       subject.place!
     end
 
-    it "translates the triangle latterally base off of the index" do
-      expect(triangle_use).to receive(:translate)
-      expect(triangle_use).to receive(:translate).with(Hexflex::R * index, 0)
-      subject.place!
-    end
-
     context "odd placement" do
       let(:indexes) { (1..19).step(2) }
 
@@ -42,6 +36,32 @@ describe Hexflex::TrianglePlacer do
       let(:indexes) { (2..18).step(2) }
       it "does not rotate triangles" do
         expect(triangle_use).to_not receive(:rotate).with(60)
+        subject.place!
+      end
+    end
+
+    context "placements 0 to 9" do
+      let(:indexes) { (0..9) }
+      it "translates the triangle latterally base off of the index" do
+        expect(triangle_use).to receive(:translate)
+        expect(triangle_use).to receive(:translate).with(Hexflex::R * index, 0)
+        subject.place!
+      end
+      it "does not place the triangle an extra triangle height below" do
+        expect(triangle_use).to_not receive(:translate).with(0, Hexflex::Y + Hexflex::R)
+        subject.place!
+      end
+    end
+
+    context "placements 10..19" do
+      let(:indexes) { (10..19) }
+      it "resets the lateral placement from the begining" do
+        expect(triangle_use).to receive(:translate)
+          .with(Hexflex::R * (index - 10), 0)
+        subject.place!
+      end
+      it "places the triangle a triangle height below" do
+        expect(triangle_use).to receive(:translate).with(0, Hexflex::Y + Hexflex::R)
         subject.place!
       end
     end
