78b3d8a63a49a5193d419e0070490128d8b33fe2
Tom Dunlap <apocryphalauthor@gmail.com>
2015-08-03 23:14:09 -0700

    Center image when too big

--- lib/hexflex/image_rvg_triangle_assembler.rb
+++ lib/hexflex/image_rvg_triangle_assembler.rb
@@ -8,9 +8,21 @@ module Hexflex
 
     def assemble!
       Magick::RVG::Group.new.tap do |group|
-        group.image(image)
+        group.use(centered_background)
           .rotate(-60 * index, base, height)
         group.styles(clip_path: clip_path)
+      end
+    end
+
+    def centered_background
+      Magick::RVG::Group.new.tap do |group|
+        group.image(image).tap do |img|
+          if image_tall?
+            img.translate(0, (hexagon_height - image_height) / 2.0)
+          else
+            img.translate((hexagon_width - image_width) / 2.0, 0)
+          end
+        end
       end
     end
 
@@ -28,10 +40,10 @@ module Hexflex
 
     def height
       @height ||=
-        if image_narrow?
+        if image_tall?
           (base / 2.0) * Math.sqrt(3)
         else
-          image.rows / 2.0
+          image_height / 2.0
         end
     end
 
@@ -40,11 +52,25 @@ module Hexflex
         if image_wide?
           (height / Math.sqrt(3)) * 2.0
         else
-          image.columns / 2.0
+          image_width / 2.0
         end
     end
 
-    private
+    def image_height
+      image.rows.to_f
+    end
+
+    def image_width
+      image.columns.to_f
+    end
+
+    def hexagon_height
+      triangle_height * 2
+    end
+
+    def hexagon_width
+      triangle_base * 2
+    end
 
     def clip_path
       Magick::RVG::ClipPath.new.tap do |path|
@@ -60,7 +86,7 @@ module Hexflex
       (image.columns / image.rows.to_f) > (2.0 / Math.sqrt(3))
     end
 
-    def image_narrow?
+    def image_tall?
       !image_wide?
     end
   end
