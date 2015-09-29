329a742a39bcd253138def0fa75c6b856602a63c
Tom Dunlap <tdunlap@pivotal.io>
2015-09-29 00:16:03 -0700

    Restructure test/fixture directories
    We were conflating the rake output smoke_test fixtures with the
    regression test fixtures.  This separated them out from each other.

--- Rakefile
+++ Rakefile
@@ -18,44 +18,43 @@ task :default_test do
 end
 
 task :color_test do
-  fixture_path = "#{TEST_FIXTURES}/color_test"
   Hexflex.create_template_image!(
-    output_file_name: "#{fixture_path}/color_test.png",
+    output_file_name: "color_test.png",
     side_fills: [:blue, :green, :yellow]
   )
-  puts "Color test output to #{fixture_path}/color_test.png."
+  puts "Color test output to color_test.png."
 end
 
 task :image_test do
-  fixture_path = "#{TEST_FIXTURES}/image_test"
+  fixture_path = "#{TEST_FIXTURES}/images"
   Hexflex.create_template_image!(
-    output_file_name: "#{fixture_path}/image_test.png",
+    output_file_name: "image_test.png",
     side_fills: [
       "#{fixture_path}/1.png",
       "#{fixture_path}/2.png",
       "#{fixture_path}/3.png"
     ]
   )
-  puts "Image test output to #{fixture_path}/image_test.png."
+  puts "Image test output to image_test.png."
 end
 
 task :photo_test do
-  fixture_path = "#{TEST_FIXTURES}/photo_test"
+  fixture_path = "#{TEST_FIXTURES}/photos"
   Hexflex.create_template_image!(
-    output_file_name: "#{fixture_path}/photo_test.png",
+    output_file_name: "photo_test.png",
     side_fills: [
       "#{fixture_path}/1.jpg",
       "#{fixture_path}/2.jpg",
       "#{fixture_path}/3.jpg"
     ]
   )
-  puts "Photo test output to #{fixture_path}/photo_test.png."
+  puts "Photo test output to photo_test.png."
 end
 
 task :glue_test do
-  fixture_path = "#{TEST_FIXTURES}/photo_test"
+  fixture_path = "#{TEST_FIXTURES}/photos"
   Hexflex.create_template_image!(
-    output_file_name: "#{TEST_FIXTURES}/glue_test.png",
+    output_file_name: "glue_test.png",
     side_fills: [
       "#{fixture_path}/1.jpg",
       "#{fixture_path}/2.jpg",
@@ -63,5 +62,5 @@ task :glue_test do
     ],
     template: :glue
   )
-  puts "Photo test output to #{TEST_FIXTURES}/glue_test.png."
+  puts "Photo test output to glue_test.png."
 end
--- spec/lib/hexflex_spec.rb
+++ spec/lib/hexflex_spec.rb
@@ -9,14 +9,8 @@ describe Hexflex do
       end
 
       failure_message do |vector|
-        "expected that the generated output be the same as template\n" +
-          "[mean_error_per_pixel, normalized_mean_error, normalized_maximum_error]\n" +
-          image_difference(fixture, vector).inspect
-      end
-
-      failure_message_when_negated do |vector|
-        "expected that the generated output be different from the template\n" +
-          image_difference(fixture, vector).inspect
+        "expected that the generated output be the same as template" +
+          debug_output(fixture, vector)
       end
 
       def render_similar(fixture, vector)
@@ -25,6 +19,20 @@ describe Hexflex do
 
       def image_difference(fixture, vector)
         fixture.difference(vector.draw)
+      end
+
+      def debug_output(fixture, vector)
+        debug_image_path =
+          "tmp/" +
+          RSpec.current_example.full_description.parameterize('_') +
+          "_#{Time.current.to_i}.png"
+        vector.draw.write(debug_image_path)
+
+        "\nDEBUG OUTPUT:\n" +
+          "[mean_error_per_pixel, normalized_mean_error, normalized_maximum_error]\n" +
+          image_difference(fixture, vector).inspect + "\n" +
+          "Take a look for yourself:\n" +
+          debug_image_path
       end
     end
 
@@ -42,7 +50,7 @@ describe Hexflex do
 
     context 'with no opts passed in' do
       subject { described_class.make_template_vector }
-      let(:expected_image_path) { 'out.png' }
+      let(:expected_image_path) { 'spec/fixtures/default_out.png' }
 
       it_behaves_like 'a properly prepared template vector'
     end
@@ -53,7 +61,7 @@ describe Hexflex do
           side_fills: [ 'blue', 'green', 'yellow' ]
         )
       end
-      let(:expected_image_path) { 'spec/fixtures/color_test/color_test.png' }
+      let(:expected_image_path) { 'spec/fixtures/color_out.png' }
 
       it_behaves_like 'a properly prepared template vector'
     end
@@ -62,13 +70,13 @@ describe Hexflex do
       subject do
         described_class.make_template_vector(
           side_fills: [
-            'spec/fixtures/image_test/1.png',
-            'spec/fixtures/image_test/2.png',
-            'spec/fixtures/image_test/3.png'
+            'spec/fixtures/images/1.png',
+            'spec/fixtures/images/2.png',
+            'spec/fixtures/images/3.png'
           ]
         )
       end
-      let(:expected_image_path) { 'spec/fixtures/image_test/image_test.png' }
+      let(:expected_image_path) { 'spec/fixtures/image_out.png' }
 
       it_behaves_like 'a properly prepared template vector'
     end
@@ -77,13 +85,13 @@ describe Hexflex do
       subject do
         described_class.make_template_vector(
           side_fills: [
-            'spec/fixtures/photo_test/1.jpg',
-            'spec/fixtures/photo_test/2.jpg',
-            'spec/fixtures/photo_test/3.jpg'
+            'spec/fixtures/photos/1.jpg',
+            'spec/fixtures/photos/2.jpg',
+            'spec/fixtures/photos/3.jpg'
           ]
         )
       end
-      let(:expected_image_path) { 'spec/fixtures/photo_test/photo_test.png' }
+      let(:expected_image_path) { 'spec/fixtures/photo_out.png' }
 
       it_behaves_like 'a properly prepared template vector'
     end
