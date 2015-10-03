66ea709700490b44f71c519117be63fb84ca4d26
Tom Dunlap <tdunlap@pivotal.io>
2015-10-03 16:38:31 -0700

    Refactor out constants for default template

--- lib/hexflex.rb
+++ lib/hexflex.rb
@@ -7,16 +7,17 @@ require "hexflex/hexaflexagon"
 module Hexflex
 
   class << self
-    def make_template_vector(side_fills: [:cyan, :magenta, :yellow], template: :tape)
+
+    DEFAULT_SIDE_FILLS = [:cyan, :magenta, :yellow]
+    DEFAULT_TEMPLATE = :tape
+
+    def make_template_vector(side_fills: DEFAULT_SIDE_FILLS, template: DEFAULT_TEMPLATE)
       template = Hexaflexagon.new(side_fills).as_template(template)
       template.make_vector
     end
 
-    def create_template_image!(output_file_name: 'out.png', side_fills: nil, template: nil)
-      hexflex_opts = {}
-      hexflex_opts[:side_fills] = side_fills unless side_fills.nil?
-      hexflex_opts[:template] = template unless template.nil?
-      vector = make_template_vector(hexflex_opts)
+    def create_template_image!(output_file_name: 'out.png', side_fills: DEFAULT_SIDE_FILLS, template: DEFAULT_TEMPLATE)
+      vector = make_template_vector(side_fills: side_fills, template: template)
       vector.draw.write(output_file_name)
     end
 
